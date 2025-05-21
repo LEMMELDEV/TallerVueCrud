502930683f14db3723ef1cb946fea1959c3ddc2c
LEMMEL <LEMMEL@LEMMEL>
2025-05-20 20:46:31 -0500

    Ajuste rutas

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,38 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import Comunas from '@/views/Comunas.vue'
+
+import EditarComuna from '@/components/Comuna/EditarComuna.vue'
+import NewComuna from '@/components/Comuna/NewComuna.vue'
+import Comunas from '../views/Comunas.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
+    name: 'Home',
     component: HomeView
   },
   {
+    path: '/comunas',
+    name: 'Comunas',
+    component: Comunas
+  },
+  {
+    path: '/editar-comuna/:id',
+    name: 'EditarComuna',
+    component: EditarComuna
+  },
+  {
+    path: '/add-comuna/',
+    name: 'NewComuna',
+    component: NewComuna
+  },
+  {
     path: '/about',
-    name: 'about',
+    name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
-    path: '/comunas',
-    name : 'comunas',
-    component: () => import ('../views/Comunas.vue')
   }
 ]
 
